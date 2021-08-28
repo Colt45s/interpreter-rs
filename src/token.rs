deeb8065c2ada6829860f34611f3ba82b1637710
Colt45s <kzk0693@gmail.com>
2021-08-28 23:42:36 +0900

    add token

---
+++ src/token.rs
@@ -0,0 +1,16 @@
+pub enum Token {
+    ILLEGAL,
+    EOF,
+    IDENT,
+    INT,
+    ASSIGN,
+    PLUS,
+    COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    FUNCTION,
+    LET,
+}
