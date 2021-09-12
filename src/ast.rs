12386e1bcd02b3aa2d5592a22dad438de74be93c
Colt45s <kzk0693@gmail.com>
2021-09-12 23:16:45 +0900

    fix ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,24 @@
+#[derive(Debug, PartialEq, Clone)]
 pub struct Program {
     pub statements: Vec<Statement>,
 }
 
-type Ident = String;
+#[derive(Debug, PartialEq, Clone)]
 pub enum Statement {
     Let(Ident, Expression)
 }
 
-enum Expression {
+#[derive(Debug, PartialEq, Clone)]
+pub struct Ident(pub String);
 
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    Ident(Ident),
+    Literal(Literal)
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Literal {
+    Int(i32)
 }
