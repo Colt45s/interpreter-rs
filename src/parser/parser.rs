14c4762a9ae195d01723f36c8ec263c3b91ca928
Colt45s <kzk0693@gmail.com>
2021-09-19 22:51:32 +0900

    add parse expression

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,6 +1,17 @@
 use super::ast;
 use crate::lexer::lexer::Lexer;
 use crate::lexer::token::Token;
+use anyhow::Result;
+
+enum Priority {
+    Lowest,
+    Equals,       // ==
+    LessGreater,  // > or <
+    Sum,          // +
+    Product,      // *
+    Prefix,       // -X or !X
+    Call          // myFunction(X)
+}
 
 struct Parser<'a> {
     lexer: Lexer<'a>,
@@ -27,13 +38,13 @@ impl <'a> Parser<'a> {
           self.peek_token = self.lexer.next_token()  
     }
 
-    fn expect_peek(&mut self, token: Token) -> Result<(), String> {
+    fn expect_peek(&mut self, token: Token) -> Result<()> {
         match self.peek_token_is(&token) {
             true => {
                 self.next_token();
                 Ok(())
             },
-            false => return Err(format!("Expect token {0}. But received {1}", token, self.peek_token))
+            false => return Err(anyhow::anyhow!(format!("Expect token {0}. But received {1}", token, self.peek_token)))
         }
     }
 
@@ -41,38 +52,29 @@ impl <'a> Parser<'a> {
         self.peek_token == *t
     }
 
-    pub fn parse_program(&mut self) -> Result<ast::Program, String> {
+    pub fn parse_program(&mut self) -> Result<ast::Program> {
         let mut program = ast::Program::default();
 
         while self.current_token != Token::EOF {
-            let statement = self.parse_statement();
-            match statement {
-                Some(s) => {
-                    program.statements.push(s);
-                },
-                None => {}
+            if let Ok(statement) = self.parse_statement() {
+                program.statements.push(statement);                
             }
+
             self.next_token();
         }
 
         Ok(program)
     }
 
-    fn parse_statement(&mut self) -> Option<ast::Statement> {
+    fn parse_statement(&mut self) -> Result<ast::Statement> {
         match self.current_token {
-            Token::Let => {
-                let statement = self.parse_let_statement().unwrap();
-                Some(statement)
-            },
-            Token::Return => {
-                let statement = self.parse_return_statement().unwrap();
-                Some(statement)
-            },
-            _ => return None
+            Token::Let => self.parse_let_statement(),
+            Token::Return => self.parse_return_statement(),
+            _ => self.parse_expression_statement()
         }
     }
 
-    fn parse_let_statement(&mut self) ->  Result<ast::Statement, String> {
+    fn parse_let_statement(&mut self) ->  Result<ast::Statement> {
         let name = self.parse_indent()?;
 
         self.expect_peek(Token::Assign)?;
@@ -80,7 +82,7 @@ impl <'a> Parser<'a> {
         self.next_token();
         let literal = match self.current_token {
             Token::Int(l) =>  ast::Expression::Literal(ast::Literal::Int(l)),
-            _ => return Err(format!("Invalid token {}", self.current_token))
+            _ => return Err(anyhow::anyhow!(format!("Invalid token {}", self.current_token)))
         };
 
         while !self.peek_token_is(&Token::Semicolon) {
@@ -90,11 +92,11 @@ impl <'a> Parser<'a> {
         Ok(ast::Statement::Let(name, literal))
     }
 
-    fn parse_return_statement(&mut self) ->  Result<ast::Statement, String> {
+    fn parse_return_statement(&mut self) ->  Result<ast::Statement> {
         self.next_token();
         let literal = match self.current_token {
             Token::Int(l) =>  ast::Expression::Literal(ast::Literal::Int(l)),
-            _ => return Err(format!("Invalid token {}", self.current_token))
+            _ => return Err(anyhow::anyhow!(format!("Invalid token {}", self.current_token)))
         };
 
         while !self.peek_token_is(&Token::Semicolon) {
@@ -104,14 +106,40 @@ impl <'a> Parser<'a> {
         Ok(ast::Statement::Return(literal))
     }
 
-    fn parse_indent(&mut self) -> Result<ast::Identifier, String> {
+    fn parse_indent(&mut self) -> Result<ast::Identifier> {
         let ident = match &self.peek_token {
             Token::Ident(i) => ast::Identifier(i.to_string()),
-            _ => return Err(format!("Invalid identifier {}", self.peek_token))
+            _ => return Err(anyhow::anyhow!(format!("Invalid identifier {}", self.peek_token)))
         };
 
         self.next_token();
         Ok(ident)
+    }
+
+    fn parse_expression_statement(&mut self) -> Result<ast::Statement> {
+        let expr = self.parse_expression(Priority::Lowest)?;
+
+        if self.peek_token_is(&Token::Semicolon) {
+            self.next_token();
+        }
+
+        Ok(ast::Statement::Expr(expr))
+    }
+
+    fn parse_expression(&mut self, priority: Priority) -> Result<ast::Expression> {
+        let prefix = match self.current_token {
+            Token::Ident(_) => ast::Expression::Ident(self.parse_identifier()?),
+            _ => return Err(anyhow::anyhow!("Not implement"))
+        };
+        Ok(prefix)
+    }
+
+    fn parse_identifier(&mut self) -> Result<ast::Identifier> {
+        if let Token::Ident(ident) = &self.current_token {
+            Ok(ast::Identifier(ident.clone()))
+        } else {
+            Err(anyhow::anyhow!(format!("Invalid token {}", self.current_token)))
+        }
     }
 }
 
@@ -165,6 +193,17 @@ mod tests {
             } else {
                 panic!();
             }
+        }
+    }
+
+    #[test]
+    fn test_identifier_expression() {
+        let input = ("foobar;", "foobar".to_string());
+        let program = parse(input.0);
+        assert!(program.statements.len() > 0);
+        let target = program.statements.get(0);
+        if let Some(ast::Statement::Expr(ast::Expression::Ident(identifier))) = target {
+            assert_eq!(*identifier, ast::Identifier(input.1));
         }
     }
 }
