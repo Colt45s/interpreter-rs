9ae385308658ae05aaf79f888c821a19ae6af400
Colt45s <kzk0693@gmail.com>
2021-09-25 01:25:44 +0900

    update parser

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -22,12 +22,12 @@ struct Parser<'a> {
 
 #[derive(Error, Debug)]
 pub enum ParserError {
-    #[error("expect peek token (expected {expected:?}, found {found:?})")]
-    ExpectPeek { expected: Token, found: Token },
-    #[error("expect identifier token. {0}")]
-    ExpectIdentifier(String),
+    #[error("expect token (expected {expected:?}, found {found:?})")]
+    ExpectToken { expected: String, found: String },
     #[error("expect expression token. {0}")]
     ExpectExpression(String),
+    #[error("unable to parse integer. {0}")]
+    UnableToParseInteger(String),
 }
 
 type Result<T> = std::result::Result<T, ParserError>;
@@ -57,9 +57,9 @@ impl<'a> Parser<'a> {
             self.next_token();
             Ok(())
         } else {
-            Err(ParserError::ExpectPeek {
-                expected: token,
-                found: self.peek_token.clone(),
+            Err(ParserError::ExpectToken {
+                expected: format!("{}", token),
+                found: format!("{}", self.peek_token.clone()),
             })
         }
     }
@@ -109,16 +109,15 @@ impl<'a> Parser<'a> {
     fn parse_let_statement(&mut self) -> Result<ast::Statement> {
         self.expect_peek(Token::Ident("_".to_string()))?;
 
-        let name = self.parse_identifier()?;
+        let name = ast::Identifier(match &self.current_token {
+            Token::Ident(ident) => ident.clone(),
+            _ => unreachable!(),
+        });
 
         self.expect_peek(Token::Assign)?;
 
         self.next_token();
-        let literal = match &self.current_token {
-            Token::Int(l) => ast::Expression::Literal(ast::Literal::Int(*l)),
-            t => return Err(ParserError::ExpectExpression(format!("{}", t))),
-        };
-
+        let literal = self.parse_expression(Priority::Lowest)?;
         while !self.peek_token_is(&Token::Semicolon) {
             self.next_token();
         }
@@ -128,11 +127,7 @@ impl<'a> Parser<'a> {
 
     fn parse_return_statement(&mut self) -> Result<ast::Statement> {
         self.next_token();
-        let literal = match &self.current_token {
-            Token::Int(l) => ast::Expression::Literal(ast::Literal::Int(*l)),
-            t => return Err(ParserError::ExpectExpression(format!("{}", t))),
-        };
-
+        let literal = self.parse_expression(Priority::Lowest)?;
         while !self.peek_token_is(&Token::Semicolon) {
             self.next_token();
         }
@@ -152,17 +147,36 @@ impl<'a> Parser<'a> {
 
     fn parse_expression(&mut self, priority: Priority) -> Result<ast::Expression> {
         let prefix = match &self.current_token {
-            Token::Ident(_) => ast::Expression::Ident(self.parse_identifier()?),
+            Token::Ident(_) => self.parse_identifier()?,
+            Token::Int(_) => self.parse_integer_literal()?,
             t => return Err(ParserError::ExpectExpression(format!("{}", t))),
         };
-        Ok(prefix)
+        Ok(prefix.clone())
     }
 
-    fn parse_identifier(&mut self) -> Result<ast::Identifier> {
-        Ok(match &self.current_token {
-            Token::Ident(ident) => ast::Identifier(ident.clone()),
-            t => Err(ParserError::ExpectIdentifier(format!("{}", t)))?,
-        })
+    fn parse_identifier(&mut self) -> Result<ast::Expression> {
+        let val = match &self.current_token {
+            Token::Ident(ident) => ident.clone(),
+            t => Err(ParserError::ExpectToken {
+                expected: "Ident".to_string(),
+                found: format!("{}", t),
+            })?,
+        };
+
+        Ok(ast::Expression::Ident(ast::Identifier(val)))
+    }
+
+    fn parse_integer_literal(&mut self) -> Result<ast::Expression> {
+        let parsed = match &self.current_token {
+            Token::Int(val) => val.clone().parse::<i32>().map_err(|_| {
+                ParserError::UnableToParseInteger(format!("{}", self.current_token))
+            })?,
+            t => Err(ParserError::ExpectToken {
+                expected: "Int".to_string(),
+                found: format!("{}", t),
+            })?,
+        };
+        Ok(ast::Expression::IntegerLiteral(parsed))
     }
 }
 
@@ -181,12 +195,20 @@ mod tests {
     #[test]
     fn test_let_statement() {
         let inputs = vec![
-            ("let x = 5;", String::from("x"), ast::Literal::Int(5)),
-            ("let y = 10;", String::from("y"), ast::Literal::Int(10)),
+            (
+                "let x = 5;",
+                String::from("x"),
+                ast::Expression::IntegerLiteral(5),
+            ),
+            (
+                "let y = 10;",
+                String::from("y"),
+                ast::Expression::IntegerLiteral(10),
+            ),
             (
                 "let foobar = 838383;",
                 String::from("foobar"),
-                ast::Literal::Int(838383),
+                ast::Expression::IntegerLiteral(838383),
             ),
         ];
 
@@ -196,7 +218,7 @@ mod tests {
             let target = program.statements.get(0);
             if let Some(ast::Statement::Let(ident, exp)) = target {
                 assert_eq!(*ident, ast::Identifier(expect_ident_value));
-                assert_eq!(*exp, ast::Expression::Literal(expect_literal_value));
+                assert_eq!(*exp, expect_literal_value);
             } else {
                 panic!();
             }
@@ -206,9 +228,9 @@ mod tests {
     #[test]
     fn test_return_statement() {
         let inputs = vec![
-            ("return 5;", ast::Literal::Int(5)),
-            ("return 10;", ast::Literal::Int(10)),
-            ("return 993322;", ast::Literal::Int(993322)),
+            ("return 5;", ast::Expression::IntegerLiteral(5)),
+            ("return 10;", ast::Expression::IntegerLiteral(10)),
+            ("return 993322;", ast::Expression::IntegerLiteral(993322)),
         ];
 
         for (input, expect_literal_value) in inputs {
@@ -216,7 +238,7 @@ mod tests {
             assert!(program.statements.len() > 0);
             let target = program.statements.get(0);
             if let Some(ast::Statement::Return(exp)) = target {
-                assert_eq!(*exp, ast::Expression::Literal(expect_literal_value));
+                assert_eq!(*exp, expect_literal_value);
             } else {
                 panic!();
             }
@@ -231,6 +253,21 @@ mod tests {
         let target = program.statements.get(0);
         if let Some(ast::Statement::Expr(ast::Expression::Ident(identifier))) = target {
             assert_eq!(*identifier, ast::Identifier(input.1));
+        } else {
+            panic!();
+        }
+    }
+
+    #[test]
+    fn test_integer_literal_expression() {
+        let input = ("5;", 5);
+        let program = parse(input.0);
+        assert!(program.statements.len() > 0);
+        let target = program.statements.get(0);
+        if let Some(ast::Statement::Expr(ast::Expression::IntegerLiteral(int))) = target {
+            assert_eq!(*int, input.1);
+        } else {
+            panic!();
         }
     }
 }
